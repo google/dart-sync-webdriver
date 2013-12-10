ffbcf5b2754cdddad95e808ff8bbce7e4244942e
sestegra <sestegra@gmail.com>
2013-12-10 15:39:05 +0100

    Helper class to select and deselect options

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -114,3 +114,266 @@ class _HasText extends Matcher {
         .addDescriptionOf(item.text);
   }
 }
+
+// TODO Exception management
+class UnexpectedTagNameException extends FormatException {
+  UnexpectedTagNameException(String expectedTagName, String actualTagName)
+    : super('Element should have been "$expectedTagName" but was "$actualTagName"');
+}
+
+/**
+ * Models a SELECT tag, providing helper methods to select and deselect options.
+ */
+class SelectUtil {
+  WebElement _element;
+  bool _isMulti;
+  
+  /**
+   * A check is made that the given [element] is, indeed, a SELECT tag.
+   * If it is not, then an [UnexpectedTagNameException] is thrown.
+   */
+  SelectUtil(WebElement element) {
+    var tagName = element.name;
+
+    if (null == tagName || tagName.toLowerCase() != "select") {
+      throw new UnexpectedTagNameException("select", tagName);
+    }
+
+    _element = element;
+
+    var multi = element.attributes["multiple"];
+
+    // The atoms normalize the returned value, but check for "false"
+    _isMulti = multi != null && multi != "false";
+  }
+  
+  /**
+   * Whether this select element support selecting multiple options at the same time?
+   * This is done by checking the value of the "multiple" attribute.
+   */
+  bool get isMultiple => _isMulti;
+
+  /**
+   * All options belonging to this select tag
+   */
+  List<WebElement> get options => _element.findElements(new By.tagName("option"));
+
+  /**
+   * All selected options belonging to this select tag
+   */
+  List<WebElement> get allSelectedOptions => options.where((option) => option.selected);
+
+  /**
+   * The first selected option in this select tag (or the currently selected 
+   * option in a normal select)
+   */
+  // TODO Exception management
+  WebElement get firstSelectedOption => options.firstWhere((option) => option.selected);
+ 
+  /**
+   * Select the option at the given [index].
+   * This is done by examing the "index" attribute of an element, and not merely by counting.
+   */
+  selectByIndex(int index) {
+    var match = index.toString();
+    
+    var matched = false;
+    for (var option in options) {
+      if (option.attributes["index"] == match) {
+        _setSelected(option);
+        if (!isMultiple) {
+          return;
+        }
+        matched = true;
+      }
+    }
+    
+    if (!matched) {
+      // TODO Exception management
+      throw new NoSuchElementException(0, "Cannot locate option with index: $index");
+    }
+  }
+
+  /**
+   * Select all options that have a [value] matching the argument.
+   * That is, when given "foo" this would select an option like:
+   */
+  selectByValue(String value) {
+    var buffer = new StringBuffer(".//option[@value = ");
+    buffer.write(_escapeQuotes(value));
+    buffer.write("]");
+    var foundOptions = _element.findElements(new By.xpath(buffer.toString()));
+
+    var matched = false;
+    for (var option in foundOptions) {
+      _setSelected(option);
+      if (!isMultiple) {
+        return;
+      }
+      matched = true;
+    }
+
+    if (!matched) {
+      // TODO Exception management
+      throw new NoSuchElementException(0, "Cannot locate option with value: $value");
+    }
+  }
+
+  /**
+   * Select all options that display [text] matching the argument.
+   * That is, when given "Bar" this would select an option like:
+   * 
+   * &lt;option value="foo"&gt;Bar&lt;/option&gt;
+   */
+  void selectByVisibleText(String text) {
+    // try to find the option via XPATH ...
+    var foundOptions =
+        _element.findElements(new By.xpath(".//option[normalize-space(.) = "
+            + _escapeQuotes(text) + "]"));
+
+    var matched = false;
+    for (var option in foundOptions) {
+      _setSelected(option);
+      if (!isMultiple) {
+        return;
+      }
+      matched = true;
+    }
+
+    if (options.isEmpty && text.contains(" ")) {
+      var subStringWithoutSpace = _getLongestSubstringWithoutSpace(text);
+      var candidates;
+      if (subStringWithoutSpace == "") {
+        // hmm, text is either empty or contains only spaces - get all options ...
+        candidates = _element.findElements(new By.tagName("option"));
+      } else {
+        // get candidates via XPATH ...
+        candidates = _element.findElements(new By.xpath(".//option[contains(., " +
+                _escapeQuotes(subStringWithoutSpace) + ")]"));
+      }
+      for (var option in candidates) {
+        if (option.text == text) {
+          _setSelected(option);
+          if (!isMultiple) {
+            return;
+          }
+          matched = true;
+        }
+      }
+    }
+
+    if (!matched) {
+      // TODO Exception management
+      throw new NoSuchElementException(0, "Cannot locate element with text: " + text);
+    }
+  }
+  
+  /**
+   * Clear all selected entries.
+   * This is only valid when the SELECT supports multiple selections.
+   */
+  deselectAll() {
+    if (!isMultiple) {
+      // TODO Exception management
+      throw new Exception("You may only deselect all options of a multi-select");
+    }
+
+    for (var option in options) {
+      _unsetSelected(option);
+    }
+  }
+
+  /**
+   * Deselect the option at the given [index].
+   * This is done by examing the "index" attribute of an element, and not merely by counting.
+   */
+  deselectByIndex(int index) {
+    var match = index.toString();
+
+    for (var option in options) {
+      if (option.attributes["index"] == match) {
+        _unsetSelected(option);
+      }
+    }
+  }
+  
+  /**
+   * Deselect all options that have a [value] matching the argument.
+   * That is, when given "foo" this would deselect an option like:
+   * 
+   * &lt;option value="foo"&gt;Bar&lt;/option&gt;
+   */
+  deselectByValue(String value) {
+    var buffer = new StringBuffer(".//option[@value = ");
+    buffer.write(_escapeQuotes(value));
+    buffer.write("]");
+    var foundOptions = _element.findElements(new By.xpath(buffer.toString()));
+    for (var option in foundOptions) {
+      _unsetSelected(option);
+    }
+  }
+
+  /**
+   * Deselect all options that display [text] matching the argument.
+   * That is, when given "Bar" this would deselect an option like:
+   * 
+   * &lt;option value="foo"&gt;Bar&lt;/option&gt;
+   */
+  deselectByVisibleText(String text) {
+    var buffer = new StringBuffer(".//option[normalize-space(.) = ");
+    buffer.write(_escapeQuotes(text));
+    buffer.write("]");
+    var foundOptions = _element.findElements(new By.xpath(buffer.toString()));
+    for (var option in foundOptions) {
+      _unsetSelected(option);
+    }
+  }
+  
+  _setSelected(WebElement option) {
+    if (!option.selected) {
+      option.click();
+    }
+  }
+  
+  _unsetSelected(WebElement option) {
+    if (option.selected) {
+      option.click();
+    }
+  }
+  
+  String _escapeQuotes(String toEscape) {
+    // Convert strings with both quotes and ticks into: foo'"bar -> concat("foo'", '"', "bar")
+    if (toEscape.indexOf("\"") > -1 && toEscape.indexOf("'") > -1) {
+      var quoteIsLast = false;
+      if (toEscape.lastIndexOf("\"") == toEscape.length - 1) {
+        quoteIsLast = true;
+      }
+      var substrings = toEscape.split("\"");
+
+      var quoted = new StringBuffer("concat(");
+      for (int i = 0; i < substrings.length; i++) {
+        quoted.write("\"").append(substrings[i]).append("\"");
+        quoted.write(((i == substrings.length - 1) ? (quoteIsLast ? ", '\"')" : ")") : ", '\"', "));
+      }
+      return quoted.toString();
+    }
+
+    // Escape string with just a quote into being single quoted: f"oo -> 'f"oo'
+    if (toEscape.indexOf('"') > -1) {
+      return "'$toEscape'";
+    }
+
+    // Otherwise return the quoted string
+    return '"$toEscape"';
+  }
+  
+  String _getLongestSubstringWithoutSpace(String value) {
+    var longest = "";
+    value.split(" ").forEach((item) {
+      if (item.length > longest.length) {
+        longest = item;
+      }
+    });
+    return longest;
+  }
+}
